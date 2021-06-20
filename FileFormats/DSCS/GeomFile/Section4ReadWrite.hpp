400db7f2b01d748444ebdeace995265e795bb098
Pherakki <pherakki@gmail.com>
2021-06-21 00:00:26 +0100

    Add a dummy struct to hold data from the 4th geom section

---
+++ FileFormats/DSCS/GeomFile/Section4ReadWrite.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+
+namespace FileFormats::DSCS::GeomFile
+{
+	typedef uint16_t FakeFloat16_t;
+	
+	struct Sec4ReadWrite
+	{
+		uint16_t unknown_0x00 = 0;
+		uint16_t unknown_0x02 = 0;
+		uint16_t unknown_0x04 = 0;
+		uint16_t unknown_0x06 = 0;
+
+		uint16_t unknown_0x08 = 0;
+		FakeFloat16_t unknown_0x0A = 0;
+		uint16_t unknown_0x0C = 0;
+		FakeFloat16_t unknown_0x0E = 0;
+
+		float unknown_0x10 = 0.f;
+		float unknown_0x14 = 0.f;
+		float unknown_0x18 = 0.f;
+		float unknown_0x1C = 0.f;
+
+		uint16_t unknown_0x20 = 0;
+		uint16_t unknown_0x22 = 0;
+		uint16_t unknown_0x24 = 0;
+		uint16_t unknown_0x26 = 0;
+		uint16_t unknown_0x28 = 0;
+		uint16_t unknown_0x2A = 0;
+		uint32_t unknown_0x2C = 0;
+
+		uint64_t unknown_0x30 = 0;
+		uint64_t unknown_0x38 = 0;
+	};
+}
