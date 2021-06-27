14f1afdf589d1d0787b408e4949c318057356f53
Pherakki <pherakki@gmail.com>
2021-06-27 02:22:45 +0100

    Add copy, assignment, and move constructors

--- FileFormats/DSCS/GeomFile/MeshReadWrite.hpp
+++ FileFormats/DSCS/GeomFile/MeshReadWrite.hpp
@@ -90,10 +90,78 @@ namespace FileFormats::DSCS::GeomFile
         MeshReadWrite() {};
         ~MeshReadWrite() {};
         friend void swap(MeshReadWrite& first, MeshReadWrite& second);
+        MeshReadWrite(const MeshReadWrite&);
+        MeshReadWrite& operator=(MeshReadWrite other)
+        {
+            swap(*this, other);
+            return *this;
+        }
+        MeshReadWrite(MeshReadWrite&& other) noexcept
+            : MeshReadWrite()
+        {
+            swap(*this, other);
+        }
 
         // Friends
         friend class GeomReadWrite;
+        friend class DSCStoOpenGL;
+
 	};
+
+    MeshReadWrite::MeshReadWrite(const MeshReadWrite& other)
+    {
+        // Header Variables
+        vertex_data_ptr = other.vertex_data_ptr;
+        triangles_ptr = other.triangles_ptr;
+        skinning_indices_ptr = other.skinning_indices_ptr;
+        vertex_attribute_definitions_ptr = other.vertex_attribute_definitions_ptr;
+        num_skinning_indices = other.num_skinning_indices;
+        num_vertex_attributes = other.num_vertex_attributes;
+        bytes_per_vertex = other.bytes_per_vertex;
+        max_skinning_indices_per_vertex = other.max_skinning_indices_per_vertex;
+        unknown_0x31 = other.unknown_0x31;
+        triangle_data_type = other.triangle_data_type;
+        name_hash = other.name_hash;
+        material_id = other.material_id;
+        num_vertices = other.num_vertices;
+        num_elements_in_index_buffer = other.num_elements_in_index_buffer;
+        bounding_box_diagonal = other.bounding_box_diagonal;
+        mesh_centre = other.mesh_centre;
+        bounding_box_lengths = other.bounding_box_lengths;
+
+        // Data Holders
+        vertices = other.vertices;
+        skinning_indices = other.skinning_indices;
+        index_buffer = other.index_buffer;
+        vertex_attributes = other.vertex_attributes;
+    }
+
+    void swap(MeshReadWrite& first, MeshReadWrite& second)
+    {
+        using std::swap;
+        swap(first.vertex_data_ptr, second.vertex_data_ptr);
+        swap(first.triangles_ptr, second.triangles_ptr);
+        swap(first.skinning_indices_ptr, second.skinning_indices_ptr);
+        swap(first.vertex_attribute_definitions_ptr, second.vertex_attribute_definitions_ptr);
+        swap(first.num_skinning_indices, second.num_skinning_indices);
+        swap(first.num_vertex_attributes, second.num_vertex_attributes);
+        swap(first.bytes_per_vertex, second.bytes_per_vertex);
+        swap(first.max_skinning_indices_per_vertex, second.max_skinning_indices_per_vertex);
+        swap(first.unknown_0x31, second.unknown_0x31);
+        swap(first.triangle_data_type, second.triangle_data_type);
+        swap(first.name_hash, second.name_hash);
+        swap(first.material_id, second.material_id);
+        swap(first.bounding_box_diagonal, second.bounding_box_diagonal);
+        swap(first.mesh_centre, second.mesh_centre);
+        swap(first.bounding_box_lengths, second.bounding_box_lengths);
+
+        swap(first.vertices, second.vertices);
+        swap(first.skinning_indices, second.skinning_indices);
+        swap(first.index_buffer, second.index_buffer);
+        swap(first.vertex_attributes, second.vertex_attributes);
+    }
+
+
 
     // **************************************** //
     // **************************************** //
