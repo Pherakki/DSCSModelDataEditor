bda854445ca6e4e05630b777bb489080bff1bc46
Pherakki <pherakki@gmail.com>
2021-06-27 02:22:58 +0100

    Update types

--- FileFormats/DSCS/GeomFile/Section4ReadWrite.hpp
+++ FileFormats/DSCS/GeomFile/Section4ReadWrite.hpp
@@ -10,25 +10,20 @@ namespace FileFormats::DSCS::GeomFile
 	{
 		uint16_t unknown_0x00 = 0;
 		uint16_t unknown_0x02 = 0;
-		uint16_t unknown_0x04 = 0;
+		uint16_t flag =         0;
 		uint16_t unknown_0x06 = 0;
 
-		uint16_t unknown_0x08 = 0;
-		FakeFloat16_t unknown_0x0A = 0;
-		uint16_t unknown_0x0C = 0;
-		FakeFloat16_t unknown_0x0E = 0;
+		float unknown_0x08 = 0.f;
+		float unknown_0x0C = 0.f;
 
 		float unknown_0x10 = 0.f;
 		float unknown_0x14 = 0.f;
 		float unknown_0x18 = 0.f;
 		float unknown_0x1C = 0.f;
 
-		uint16_t unknown_0x20 = 0;
-		uint16_t unknown_0x22 = 0;
-		uint16_t unknown_0x24 = 0;
-		uint16_t unknown_0x26 = 0;
-		uint16_t unknown_0x28 = 0;
-		uint16_t unknown_0x2A = 0;
+		float unknown_0x20 = 0;
+		float unknown_0x24 = 0;
+		float unknown_0x28 = 0;
 		uint32_t unknown_0x2C = 0;
 
 		uint64_t unknown_0x30 = 0;
