2a1db8d336e56a3bb77413b1aeff8aee7b491274
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-03 22:49:18 +0100

    Fix whitespace

--- src/UI/Types.hpp
+++ src/UI/Types.hpp
@@ -4,5 +4,5 @@
 #include "../Rendering/DSCS/DataObjects/OpenGLDSCSModel.hpp"
 
 typedef int32_t ModelID_t;
-typedef std::map <ModelID_t, Rendering::DSCS::DataObjects::OpenGLDSCSModel > ModelList_t;
+typedef std::map <ModelID_t, Rendering::DSCS::DataObjects::OpenGLDSCSModel> ModelList_t;
 static const ModelID_t null_model_id = (ModelID_t)(-1);
