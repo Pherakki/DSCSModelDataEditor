0cc9dc30df8c39fc746239f00b63dde83c3dccc8
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-04 22:37:44 +0100

    Remove redundant static and make ModelList_t more efficient

--- src/UI/Types.hpp
+++ src/UI/Types.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <map>
+#include <unordered_map>
 #include "../Rendering/DSCS/DataObjects/OpenGLDSCSModel.hpp"
 
 typedef int32_t ModelID_t;
-typedef std::map <ModelID_t, Rendering::DSCS::DataObjects::OpenGLDSCSModel> ModelList_t;
-static const ModelID_t null_model_id = (ModelID_t)(-1);
+typedef std::unordered_map<ModelID_t, Rendering::DSCS::DataObjects::OpenGLDSCSModel> ModelList_t;
+const ModelID_t null_model_id = (ModelID_t)(-1);
