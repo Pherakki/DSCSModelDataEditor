d70346c10ee4f9b742ef73a20a8197e2a72de723
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-08 21:41:56 +0100

    Display materials on the MeshEditor also

--- src/UI/EditorTabs/MeshEditor.hpp
+++ src/UI/EditorTabs/MeshEditor.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <QtWidgets/QWidget>
+#include <QtWidgets/QBoxLayout>
 #include <QtWidgets/QListWidget>
+#include <QtWidgets/QWidget>
 
 #include "../Types.hpp"
 #include "../../Rendering/DSCS/DataObjects/OpenGLDSCSMesh.hpp"
@@ -19,17 +20,23 @@ private:
 
     ModelPtr    selected_model = nullptr;
     QListWidget* mesh_list;
+    QListWidget* material_list;
 
 public:
     MeshEditorTab(QWidget* parent = Q_NULLPTR) : QWidget(parent)
     {
+        auto layout = new QHBoxLayout(this);
         this->mesh_list = new QListWidget(this);
+        this->material_list = new QListWidget(this);
+        layout->addWidget(this->mesh_list);
+        layout->addWidget(this->material_list);
+        this->setLayout(layout);
     };
 
-
-    void updateMeshList()
+    void updateDataList()
     {
         this->mesh_list->clear();
+        this->material_list->clear();
         if (this->selected_model)
         {
             for (size_t i = 0; i < this->selected_model->meshes.size(); ++i)
@@ -37,12 +44,18 @@ public:
                 auto list_item = new QListWidgetItem(QString::fromStdString("Mesh " + std::to_string(i)));
                 this->mesh_list->addItem(list_item);
             }
+            for (size_t i = 0; i < this->selected_model->materials.size(); ++i)
+            {
+                auto list_item = new QListWidgetItem(QString::fromStdString(this->selected_model->materials[i]->name));
+                this->material_list->addItem(list_item);
+            }
         }
     }
+
 public slots:
     void updateSelectedModel(ModelPtr model_ptr)
     {
         this->selected_model = model_ptr;
-        this->updateMeshList();
+        this->updateDataList();
     }
 };
