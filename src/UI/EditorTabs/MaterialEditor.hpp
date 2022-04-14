4674ef3f275a743a7ed2c090d42039f02741fedb
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-14 22:15:50 +0100

    Add UI for OpenGL Settings

--- src/UI/EditorTabs/MaterialEditor.hpp
+++ src/UI/EditorTabs/MaterialEditor.hpp
@@ -3,13 +3,147 @@
 #include <unordered_map>
 
 #include <QtWidgets/QBoxLayout>
+#include <QtWidgets/QCheckBox>
 #include <QtWidgets/QComboBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QWidget>
 
 #include "../Types.hpp"
 #include "../../Rendering/DSCS/DataObjects/OpenGLDSCSMaterial.hpp"
 #include "ShaderEditors/ShaderEditorTabs.hpp"
 
+class OpenGLSettingsWidget : public QWidget
+{
+    Q_OBJECT
+private:
+    QCheckBox* alphafunc_checkbox  = new QCheckBox(this);
+    QCheckBox* alphatest_checkbox  = new QCheckBox(this);
+    QCheckBox* blend_checkbox      = new QCheckBox(this);
+    QCheckBox* depthtest_checkbox  = new QCheckBox(this);
+    QCheckBox* blendfunc_checkbox  = new QCheckBox(this);
+    QCheckBox* blendeq_checkbox    = new QCheckBox(this);
+    QCheckBox* facerender_checkbox = new QCheckBox(this);
+    QCheckBox* depthfunc_checkbox  = new QCheckBox(this);
+    QCheckBox* depthmask_checkbox  = new QCheckBox(this);
+    QCheckBox* colormask_checkbox  = new QCheckBox(this);
+
+    QComboBox* alphafunc_combobox     = new QComboBox(this);
+    QComboBox* blendfunc_src_combobox = new QComboBox(this);
+    QComboBox* blendfunc_dst_combobox = new QComboBox(this);
+    QComboBox* blendeq_combobox       = new QComboBox(this);
+    QComboBox* facerender_combobox    = new QComboBox(this);
+    QComboBox* depthfunc_combobox     = new QComboBox(this);
+    QComboBox* depthmask_combobox     = new QComboBox(this);
+    QComboBox* colormask_combobox_r   = new QComboBox(this);
+    QComboBox* colormask_combobox_g   = new QComboBox(this);
+    QComboBox* colormask_combobox_b   = new QComboBox(this);
+    QComboBox* colormask_combobox_a   = new QComboBox(this);
+public:
+    OpenGLSettingsWidget(QWidget* parent = Q_NULLPTR) : QWidget(parent)
+    {
+        auto layout = new QGridLayout(this);
+
+        // Alpha Test
+        auto alphatest_label = new QLabel("Alpha Test", this);
+        layout->addWidget(this->alphatest_checkbox, 0, 0);
+        layout->addWidget(alphatest_label, 0, 1);
+
+        // Alpha Func
+        auto alphafunc_label = new QLabel("Alpha Func", this);
+        auto alphafunc_widget = new QWidget(this);
+        auto alphafunc_layout = new QHBoxLayout;
+        alphafunc_layout->addWidget(this->alphafunc_combobox);
+        alphafunc_widget->setLayout(alphafunc_layout);
+        layout->addWidget(this->alphafunc_checkbox, 1, 0);
+        layout->addWidget(alphafunc_label, 1, 1);
+        layout->addWidget(alphafunc_widget, 1, 2);
+
+        // Blend Test
+        auto blend_label = new QLabel("Blend", this);
+        auto blend_widget = new QWidget(this);
+        auto blend_layout = new QHBoxLayout;
+        blend_widget->setLayout(blend_layout);
+        layout->addWidget(this->blend_checkbox, 2, 0);
+        layout->addWidget(blend_label, 2, 1);
+        layout->addWidget(blend_widget, 2, 2);
+
+        // glBlendFunc
+        auto blendfunc_label = new QLabel("Blend Func", this);
+        auto blendfunc_widget = new QWidget(this);
+        auto blendfunc_layout = new QHBoxLayout;
+        blendfunc_layout->addWidget(this->blendfunc_src_combobox);
+        blendfunc_layout->addWidget(this->blendfunc_dst_combobox);
+        blendfunc_widget->setLayout(blendfunc_layout);
+        layout->addWidget(this->blendfunc_checkbox, 3, 0);
+        layout->addWidget(blendfunc_label, 3, 1);
+        layout->addWidget(blendfunc_widget, 3, 2);
+
+        // glBlendEquationSeparate
+        auto blendeq_label = new QLabel("Blend Equation", this);
+        auto blendeq_widget = new QWidget(this);
+        auto blendeq_layout = new QHBoxLayout;
+        blendeq_layout->addWidget(this->blendeq_combobox);
+        blendeq_widget->setLayout(blendeq_layout);
+        layout->addWidget(this->blendeq_checkbox, 4, 0);
+        layout->addWidget(blendeq_label, 4, 1);
+        layout->addWidget(blendeq_widget, 4, 2);
+
+
+        // glCullFace
+        auto facerender_label = new QLabel("Face Render", this);
+        auto facerender_widget = new QWidget(this);
+        auto facerender_layout = new QHBoxLayout;
+        facerender_layout->addWidget(this->facerender_combobox);
+        facerender_widget->setLayout(facerender_layout);
+        layout->addWidget(this->facerender_checkbox, 5, 0);
+        layout->addWidget(facerender_label, 5, 1);
+        layout->addWidget(facerender_widget, 5, 2);
+
+        // Depth Test
+        auto depthtest_label = new QLabel("Depth Test", this);
+        layout->addWidget(this->depthtest_checkbox, 6, 0);
+        layout->addWidget(depthtest_label, 6, 1);
+
+        // glDepthFunc
+        auto depthfunc_label = new QLabel("Depth Function", this);
+        auto depthfunc_widget = new QWidget(this);
+        auto depthfunc_layout = new QHBoxLayout;
+        depthfunc_layout->addWidget(this->depthfunc_combobox);
+        depthfunc_widget->setLayout(depthfunc_layout);
+        layout->addWidget(this->depthfunc_checkbox, 7, 0);
+        layout->addWidget(depthfunc_label, 7, 1);
+        layout->addWidget(depthfunc_widget, 7, 2);
+
+        // glDepthMask
+        auto depthmask_label = new QLabel("Depth Mask", this);
+        auto depthmask_widget = new QWidget(this);
+        auto depthmask_layout = new QHBoxLayout;
+        depthmask_layout->addWidget(this->depthmask_combobox);
+        depthmask_widget->setLayout(depthmask_layout);
+        layout->addWidget(this->depthmask_checkbox, 8, 0);
+        layout->addWidget(depthmask_label, 8, 1);
+        layout->addWidget(depthmask_widget, 8, 2);
+
+        // glColorMask
+        auto colormask_label = new QLabel("Color Mask", this);
+        auto colormask_widget = new QWidget(this);
+        auto colormask_layout = new QHBoxLayout;
+        colormask_layout->addWidget(this->colormask_combobox_r);
+        colormask_layout->addWidget(this->colormask_combobox_g);
+        colormask_layout->addWidget(this->colormask_combobox_b);
+        colormask_layout->addWidget(this->colormask_combobox_a);
+        colormask_widget->setLayout(colormask_layout);
+        layout->addWidget(this->colormask_checkbox, 9, 0);
+        layout->addWidget(colormask_label, 9, 1);
+        layout->addWidget(colormask_widget, 9, 2);
+
+        layout->setColumnStretch(0, 0);
+        layout->setColumnStretch(1, 0);
+        layout->setColumnStretch(2, 1);
+        this->setLayout(layout);
+    }
+};
 
 class MaterialEditorTab : public QWidget
 {
@@ -26,7 +160,7 @@ private:
     // Widgets
     QComboBox* material_dropdown;
     ShaderEditorTabs* shader_edit_modes;
-    // Third widget needs to be the OpenGL options
+    OpenGLSettingsWidget* opengl_settings;
 
     // Lookup dicts
     std::unordered_map<MaterialPtr, int> material_lookup;
@@ -38,9 +172,11 @@ public:
         auto layout = new QVBoxLayout(this);
         this->material_dropdown = new QComboBox(this);
         this->shader_edit_modes = new ShaderEditorTabs(this);
+        this->opengl_settings = new OpenGLSettingsWidget(this);
 
         layout->addWidget(this->material_dropdown);
         layout->addWidget(this->shader_edit_modes);
+        layout->addWidget(this->opengl_settings);
         this->setLayout(layout);
 
         connect(this->material_dropdown, static_cast<CIC_t>(&QComboBox::currentIndexChanged), this, &MaterialEditorTab::selectMaterial);
