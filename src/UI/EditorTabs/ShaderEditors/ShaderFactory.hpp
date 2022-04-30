0002197a267a386c428a232cff863fdedc831791
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-30 23:45:35 +0100

    Implement headers for remaining sections

--- src/UI/EditorTabs/ShaderEditors/ShaderFactory.hpp
+++ src/UI/EditorTabs/ShaderEditors/ShaderFactory.hpp
@@ -14,6 +14,7 @@
 
 #include "ShaderGenerator/VertexShader.hpp"
 #include "ShaderGenerator/FragmentShader.hpp"
+#include "../../Spoiler/Spoiler.hpp"
 #include "../../../Rendering/DSCS/DataObjects/OpenGLDSCSMesh.hpp"
 #include "../../../Rendering/DSCS/DataObjects/OpenGLDSCSMaterial.hpp"
 #include "../../../Rendering/DSCS/ShaderSystem/cgGL/cgGLShaderBackend.hpp"
@@ -60,6 +61,37 @@ public:
 		this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
 		this->toggleTextboxes(false);
 		connect(this->checkbox, &QCheckBox::stateChanged, this, &ToggleableTextboxesWidget::toggleTextboxes);
+	}
+};
+
+template <uint8_t n_boxes>
+class TextboxArrayWidget : public QWidget
+{
+public:
+	QLabel* label;
+	std::array<QLineEdit*, n_boxes> textboxes;
+
+	explicit TextboxArrayWidget(QString label_text, QWidget* parent = nullptr) : QWidget(parent)
+	{
+		auto layout = new QHBoxLayout;
+
+		this->label = new QLabel(label_text);
+		layout->addWidget(this->label);
+		layout->setAlignment(this->label, Qt::AlignLeft);
+
+		layout->addStretch();
+
+		for (auto& box : this->textboxes)
+		{
+			box = new QLineEdit;
+			box->setValidator(new QDoubleValidator(0, 100, 4, this));
+			box->setMaximumWidth(40);
+			layout->addWidget(box);
+			layout->setAlignment(box, Qt::AlignRight);
+		}
+
+		this->setLayout(layout);
+		this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
 	}
 };
 
@@ -364,6 +396,247 @@ public:
 	}
 };
 
+class TextureMapWidget : public QWidget
+{
+private:
+	void toggle(int active)
+	{
+		this->maptype->setEnabled(active);
+		this->channel->setEnabled(active);
+	}
+public:
+	QCheckBox* checkbox;
+	QLabel* label;
+	QComboBox* maptype;
+	QComboBox* channel;
+
+	TextureMapWidget(const std::string& label_text, QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		QHBoxLayout* _layout = new QHBoxLayout;
+		{
+			QHBoxLayout* title_layout = new QHBoxLayout;
+			{
+				this->checkbox = new QCheckBox(this);
+				this->label = new QLabel(QString::fromStdString(label_text), this);
+
+				title_layout->addWidget(this->checkbox, 0, Qt::AlignLeft);
+				auto spacer = new QSpacerItem(10, 1, QSizePolicy::Fixed, QSizePolicy::Fixed);
+				title_layout->addItem(spacer);
+				title_layout->addWidget(this->label, 0, Qt::AlignLeft);
+			}
+
+			QHBoxLayout* contents_layout = new QHBoxLayout;
+			QWidget* contents_widget = new QWidget(this);
+			{
+				this->maptype = new QComboBox(contents_widget);
+				this->maptype->addItem("Diffuse");
+				this->maptype->addItem("Normal");
+				this->maptype->addItem("Light");
+
+				this->channel = new QComboBox(contents_widget);
+				this->channel->addItem("R");
+				this->channel->addItem("G");
+				this->channel->addItem("B");
+				this->channel->addItem("A");
+
+				contents_layout->addWidget(this->maptype, 0, Qt::AlignRight);
+				contents_layout->addWidget(this->channel, 0, Qt::AlignRight);
+				contents_widget->setLayout(contents_layout);
+			}
+			_layout->addItem(title_layout);
+			_layout->addWidget(contents_widget, 1, Qt::AlignRight);
+		}
+		this->setLayout(_layout);
+
+		this->toggle(false);
+		connect(this->checkbox, &QCheckBox::stateChanged, this, &TextureMapWidget::toggle);
+	}
+};
+
+class ToggleableCombobox : public QWidget
+{
+private:
+	void toggle(int active)
+	{
+		this->combobox->setEnabled(active);
+	}
+public:
+	QCheckBox* checkbox;
+	QLabel* label;
+	QComboBox* combobox;
+
+	ToggleableCombobox(const std::string& label_text, QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		QHBoxLayout* _layout = new QHBoxLayout;
+		{
+			QHBoxLayout* title_layout = new QHBoxLayout;
+			{
+				this->checkbox = new QCheckBox(this);
+				this->label = new QLabel(QString::fromStdString(label_text), this);
+				title_layout->addWidget(this->checkbox, 0, Qt::AlignLeft);
+				auto verticalSpacer = new QSpacerItem(10, 1, QSizePolicy::Fixed, QSizePolicy::Fixed);
+				title_layout->addItem(verticalSpacer);
+				title_layout->addWidget(this->label, 0, Qt::AlignLeft);
+			}
+			this->combobox = new QComboBox(this);
+
+			_layout->addItem(title_layout);
+			_layout->addWidget(this->combobox, 1, Qt::AlignRight);
+		}
+		this->setLayout(_layout);
+
+		this->toggle(false);
+		connect(this->checkbox, &QCheckBox::stateChanged, this, &ToggleableCombobox::toggle);
+	}
+};
+
+class DiffuseColorSettings : public QWidget
+{
+public:
+	TitleWidget* title_widget;
+	TextboxArrayWidget<4>* diffuse_color_widget;
+	ToggleableCombobox* vertex_colors_widget;
+	TextureMapWidget* transparency_map_widget;
+	TextureMapWidget* diffuse_map_widget;
+	TextureMapWidget* diffuse_map_widget_layer_2;
+	ShaderFactoryTextureSlot* light_sampler;
+	DiffuseColorSettings(QWidget* parent=Q_NULLPTR) : QWidget(parent)
+	{
+		auto _layout = new QVBoxLayout;
+		{
+			this->title_widget = new TitleWidget("Diffuse Color", this);
+			this->diffuse_color_widget = new TextboxArrayWidget<4>("Base Color", this);
+			this->vertex_colors_widget = new ToggleableCombobox("Vertex Colors", this);
+			this->vertex_colors_widget->combobox->addItem("RGBA");
+			this->vertex_colors_widget->combobox->addItem("RGB");
+			this->transparency_map_widget = new TextureMapWidget("Transparency Map", this);
+			this->diffuse_map_widget = new TextureMapWidget("Diffuse Strength Map", this);
+			this->diffuse_map_widget_layer_2 = new TextureMapWidget("Overlay Diffuse Strength Map", this);
+			this->light_sampler = new ShaderFactoryTextureSlot("LightSampler (remember to do strength and power)", this);
+
+
+			_layout->addWidget(this->title_widget, 0, Qt::AlignTop);
+			_layout->addWidget(this->diffuse_color_widget, 0, Qt::AlignTop);
+			_layout->addWidget(this->vertex_colors_widget, 0, Qt::AlignTop);
+			_layout->addWidget(this->transparency_map_widget, 0, Qt::AlignTop);
+			_layout->addWidget(this->diffuse_map_widget, 0, Qt::AlignTop);
+			_layout->addWidget(this->diffuse_map_widget_layer_2, 0, Qt::AlignTop);
+			_layout->addWidget(this->light_sampler, 0, Qt::AlignTop);
+		}
+		this->setLayout(_layout);
+	}
+};
+
+class SpecularColorSettings : public QWidget
+{
+public:
+	TitleWidget* title_widget;
+	SpecularColorSettings(QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		auto _layout = new QVBoxLayout;
+		{
+			this->title_widget = new TitleWidget("Specular Color", this);
+
+			_layout->addWidget(this->title_widget);
+		}
+		this->setLayout(_layout);
+	}
+};
+
+class ReflectionSettings : public QWidget
+{
+public:
+	TitleWidget* title_widget;
+	ReflectionSettings(QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		auto _layout = new QVBoxLayout;
+		{
+			this->title_widget = new TitleWidget("Reflection", this);
+
+			_layout->addWidget(this->title_widget);
+		}
+		this->setLayout(_layout);
+	}
+};
+
+class IlluminationSettings : public QWidget
+{
+public:
+	TitleWidget* title_widget;
+	IlluminationSettings(QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		auto _layout = new QVBoxLayout;
+		{
+			this->title_widget = new TitleWidget("Illumination", this);
+
+			_layout->addWidget(this->title_widget);
+		}
+		this->setLayout(_layout);
+	}
+};
+
+class GlassMapSettings : public QWidget
+{
+public:
+	TitleWidget* title_widget;
+	GlassMapSettings(QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		auto _layout = new QVBoxLayout;
+		{
+			this->title_widget = new TitleWidget("Glass Map", this);
+
+			_layout->addWidget(this->title_widget);
+		}
+		this->setLayout(_layout);
+	}
+};
+
+
+class PositionSettings : public QWidget
+{
+public:
+	ToggleableTextboxesWidget<1>* fat;
+	ToggleableTextboxesWidget<1>* zbias;
+	QCheckBox* billboard_checkbox;
+	QCheckBox* billboard_align_y_checkbox;
+	PositionSettings(QWidget* parent = Q_NULLPTR) : QWidget(parent)
+	{
+		auto _layout = new QVBoxLayout;
+		{
+			this->fat = new ToggleableTextboxesWidget<1>("Fat", this);
+			this->zbias = new ToggleableTextboxesWidget<1>("Z Bias", this);
+
+			QGridLayout* billboard_layout = new QGridLayout;
+			{
+				this->billboard_checkbox = new QCheckBox(this);
+				billboard_layout->addWidget(this->billboard_checkbox, 0, 0, Qt::AlignLeft);
+				billboard_layout->addItem(new QSpacerItem(10, 1, QSizePolicy::Fixed, QSizePolicy::Fixed), 0, 1);
+				billboard_layout->addWidget(new QLabel("Billboard"), 0, 2, Qt::AlignLeft);
+				billboard_layout->setColumnStretch(0, 0);
+				billboard_layout->setColumnStretch(1, 0);
+				billboard_layout->setColumnStretch(2, 0);
+				billboard_layout->setColumnStretch(3, 1);
+				billboard_layout->addItem(new QSpacerItem(10, 1, QSizePolicy::Minimum, QSizePolicy::Expanding), 0, 3);
+				billboard_layout->addItem(new QSpacerItem(1, 10, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0, 1, 4);
+				QHBoxLayout* align_y_layout = new QHBoxLayout;
+				{
+					this->billboard_align_y_checkbox = new QCheckBox(this);
+					align_y_layout->addWidget(this->billboard_align_y_checkbox, 0, Qt::AlignLeft);
+					align_y_layout->addItem(new QSpacerItem(10, 1, QSizePolicy::Fixed, QSizePolicy::Fixed));
+					align_y_layout->addWidget(new QLabel("Align Y", this), 0, Qt::AlignLeft);
+					auto spacer = new QSpacerItem(10, 1, QSizePolicy::Minimum, QSizePolicy::Expanding);
+					align_y_layout->addItem(spacer);
+				}
+				billboard_layout->addItem(align_y_layout, 2, 2);
+			}
+			_layout->addWidget(this->fat);
+			_layout->addWidget(this->zbias);
+			_layout->addItem(billboard_layout);
+		}
+		this->setLayout(_layout);
+	}
+};
+
 struct TextureRefs
 {
 	typedef std::shared_ptr<Rendering::DataObjects::OpenGLDSCSTexture> Texture;
@@ -406,6 +679,12 @@ private:
 	ShaderFactoryUVSettingsWidget* uv_settings_1;
 	ShaderFactoryUVSettingsWidget* uv_settings_2;
 	ShaderFactoryUVSettingsWidget* uv_settings_3;
+	DiffuseColorSettings* diffuse_color_settings;
+	SpecularColorSettings* specular_color_settings;
+	ReflectionSettings* reflection_settings;
+	IlluminationSettings* illumination_settings;
+	GlassMapSettings* glassmap_settings;
+	PositionSettings* position_settings;
 
 	AnimBuf_t& animation_buffer;
 	MeshPtr selected_mesh = nullptr;
@@ -583,6 +862,7 @@ private:
 				}
 			}
 		}
+
 		createSettingInputs(settings);
 		auto vshader_text = generateVertexShader(settings);
 		auto fshader_text = generateFragmentShader(settings);
@@ -620,6 +900,12 @@ private:
 		emit this->overwriteCurrentMaterial(new_material);
 	}
 
+	void placeInSpoiler(const QString& title, QWidget* widget, QLayout* layout)
+	{
+		auto* spoiler = new Spoiler(title, 300, this);
+		spoiler->setContentLayout(*widget->layout());
+		layout->addWidget(spoiler);
+	}
 
 signals:
 	void overwriteCurrentMaterial(MaterialPtr material_ptr);
@@ -656,16 +942,29 @@ public:
 			}
 			_layout->addItem(uv_settings_layout);
 
-			auto misc_settings_layout = new QHBoxLayout;
+
+			auto contribs_layout = new QHBoxLayout;
 			{
-				auto l1 = new QLabel("Diffuse/Spec/Samplers");
-				auto l2 = new QLabel("Reflection/Glass map");
-				auto l3 = new QLabel("Illumination");
-				misc_settings_layout->addWidget(l1);
-				misc_settings_layout->addWidget(l2);
-				misc_settings_layout->addWidget(l3);
+				this->diffuse_color_settings = new DiffuseColorSettings(this);
+				contribs_layout->addWidget(this->diffuse_color_settings);
 			}
-			_layout->addItem(misc_settings_layout);
+			_layout->addItem(contribs_layout);
+
+			this->specular_color_settings = new SpecularColorSettings(this);
+			_layout->addWidget(this->specular_color_settings);
+
+			this->reflection_settings = new ReflectionSettings(this);
+			_layout->addWidget(this->reflection_settings);
+
+			this->illumination_settings = new IlluminationSettings(this);
+			_layout->addWidget(this->illumination_settings);
+
+			this->glassmap_settings = new GlassMapSettings(this);
+			_layout->addWidget(this->glassmap_settings);
+
+			this->position_settings = new PositionSettings(this);
+			//placeInSpoiler("Position Adjust", this->position_settings, _layout);
+			_layout->addWidget(this->position_settings);
 		}
 		this->setLayout(_layout);
 
