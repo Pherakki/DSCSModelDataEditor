d93424f8564a0b9e7c6a23f8d59fdcf64ca6de35
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-05-04 21:30:00 +0100

    Add methods to alter the contents of a TextureMap dropdown

--- src/UI/EditorTabs/ShaderEditors/ShaderFactory/Widgets/TextureMap.hpp
+++ src/UI/EditorTabs/ShaderEditors/ShaderFactory/Widgets/TextureMap.hpp
@@ -6,14 +6,25 @@
 #include <QLabel>
 #include <QWidget>
 
+//template <bool is_overlay>
 class TextureMapWidget : public QWidget
 {
 private:
+	QString diffuse_name = "Diffuse Texture";
+	QString normal_name = "Normal Texture";
+	QString light_name = "Light Texture";
+
 	void toggle(int active)
 	{
 		this->maptype->setEnabled(active);
 		this->channel->setEnabled(active);
 	}
+
+	void removeMap(const QString& name)
+	{
+		this->maptype->removeItem(this->maptype->findText(name));
+	}
+
 public:
 	QCheckBox* checkbox;
 	QLabel* label;
@@ -39,9 +50,6 @@ public:
 			QWidget* contents_widget = new QWidget(this);
 			{
 				this->maptype = new QComboBox(contents_widget);
-				this->maptype->addItem("Diffuse");
-				this->maptype->addItem("Normal");
-				this->maptype->addItem("Light");
 
 				this->channel = new QComboBox(contents_widget);
 				this->channel->addItem("R");
@@ -60,5 +68,35 @@ public:
 
 		this->toggle(false);
 		connect(this->checkbox, &QCheckBox::stateChanged, this, &TextureMapWidget::toggle);
+	}
+
+	void addDiffuseMap()
+	{
+		this->maptype->insertItem(0, this->diffuse_name, static_cast<uint8_t>(MapType::DIFFUSE));
+	}
+
+	void removeDiffuseMap()
+	{
+		this->removeMap(this->diffuse_name);
+	}
+
+	void addNormalMap()
+	{
+		this->maptype->insertItem(1, this->normal_name, static_cast<uint8_t>(MapType::NORMAL));
+	}
+
+	void removeNormalMap()
+	{
+		this->removeMap(this->normal_name);
+	}
+
+	void addLightMap()
+	{
+		this->maptype->insertItem(2, "Light Texture", static_cast<uint8_t>(MapType::LIGHT));
+	}
+
+	void removeLightMap()
+	{
+		this->removeMap(this->light_name);
 	}
 };
