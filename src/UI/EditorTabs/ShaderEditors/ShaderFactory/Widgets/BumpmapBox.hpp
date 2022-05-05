8c4eb831f9e82a32d8ac42571c650b539e2d3b31
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-05-05 14:42:35 +0100

    Remove tickbox from bump settings and edit toggle behaviour

--- src/UI/EditorTabs/ShaderEditors/ShaderFactory/Widgets/BumpmapBox.hpp
+++ src/UI/EditorTabs/ShaderEditors/ShaderFactory/Widgets/BumpmapBox.hpp
@@ -8,12 +8,13 @@
 
 class ShaderFactoryTextureLayerBumpmapBox : public QWidget
 {
-private:
+public:
 	void toggle(bool active)
 	{
+		this->setEnabled(active);
 		this->bump_strength->setEnabled(active);
 	}
-public:
+
 	QCheckBox* checkbox;
 	QLabel* label;
 	QLabel* bump_label;
@@ -21,32 +22,22 @@ public:
 
 	ShaderFactoryTextureLayerBumpmapBox(QWidget* parent = Q_NULLPTR) : QWidget(parent)
 	{
-		auto _layout = new QGridLayout;
+		auto settings_layout = new QVBoxLayout;
 		{
-			this->checkbox = new QCheckBox;
-			this->label = new QLabel("Bump");
-
-			_layout->addWidget(this->checkbox, 0, 0);
-			_layout->addWidget(this->label, 0, 1);
-
-			auto settings_layout = new QVBoxLayout;
+			auto bump_settings_layout = new QHBoxLayout;
 			{
-				auto bump_settings_layout = new QHBoxLayout;
-				{
-					this->bump_label = new QLabel("Strength");
-					this->bump_strength = new QLineEdit;
-					this->bump_strength->setValidator(new QDoubleValidator(0, 100, 4, this));
-					bump_settings_layout->addWidget(this->bump_label);
-					bump_settings_layout->addWidget(this->bump_strength);
-				}
-				settings_layout->addItem(bump_settings_layout);
-
+				this->bump_label = new QLabel("Bump Strength");
+				this->bump_strength = new QLineEdit;
+				this->bump_strength->setValidator(new QDoubleValidator(0, 100, 4, this));
+				this->bump_strength->setText("0.0");
+				bump_settings_layout->addWidget(this->bump_label);
+				bump_settings_layout->addWidget(this->bump_strength);
 			}
-			_layout->addLayout(settings_layout, 1, 1);
+			settings_layout->addItem(bump_settings_layout);
+
 		}
-		this->setLayout(_layout);
+		this->setLayout(settings_layout);
 
 		this->toggle(false);
-		connect(this->checkbox, &QCheckBox::stateChanged, this, &ShaderFactoryTextureLayerBumpmapBox::toggle);
 	}
 };
