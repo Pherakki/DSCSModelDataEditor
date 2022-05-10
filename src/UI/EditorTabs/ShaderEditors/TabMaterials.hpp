99296d3b4093290b370c45736a86bab5407055cf
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-05-10 20:15:52 +0100

    Create a container class for the material stored on each tab

---
+++ src/UI/EditorTabs/ShaderEditors/TabMaterials.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../../../Rendering/DSCS/DataObjects/OpenGLDSCSMaterial.hpp"
+
+struct TabMaterials
+{
+private:
+    typedef Rendering::DSCS::DataObjects::OpenGLDSCSMaterial Material;
+    typedef std::shared_ptr<Material> MaterialPtr;
+public:
+    MaterialPtr prebuilt_material = nullptr;
+    MaterialPtr factory_material = nullptr;
+    MaterialPtr custom_code_material = nullptr;
+};
