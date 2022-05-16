046bb4faedc740dede3dec839bf9acb871e05336
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-05-16 20:12:10 +0100

    Add methods to set the active material and interrogate it

--- src/UI/SelectedObjectReferences.hpp
+++ src/UI/SelectedObjectReferences.hpp
@@ -33,14 +33,22 @@ public:
 
 	}
 
-	const auto& getMaterial() const noexcept { return this->main_material; }
-	const auto& getPrebuiltMaterial  () const noexcept { return this->tab_materials.prebuilt_material   ; }
-	const auto& getFactoryMaterial   () const noexcept { return this->tab_materials.factory_material    ; }
+	const auto& getMaterial()           const noexcept { return this->main_material; }
+	const auto& getPrebuiltMaterial()   const noexcept { return this->tab_materials.prebuilt_material; }
+	const auto& getFactoryMaterial()    const noexcept { return this->tab_materials.factory_material; }
 	const auto& getCustomCodeMaterial() const noexcept { return this->tab_materials.custom_code_material; }
-	auto& getEditableMaterial() noexcept { return this->main_material; } // This needs to go in favour of automatic updates
-	auto& getEditablePrebuiltMaterial  () noexcept { return this->tab_materials.prebuilt_material   ; }
-	auto& getEditableFactoryMaterial   () noexcept { return this->tab_materials.factory_material    ; }
+	auto& getEditableMaterial()           noexcept { return this->main_material; } // This needs to go in favour of automatic updates
+	auto& getEditablePrebuiltMaterial()   noexcept { return this->tab_materials.prebuilt_material; }
+	auto& getEditableFactoryMaterial()    noexcept { return this->tab_materials.factory_material; }
 	auto& getEditableCustomCodeMaterial() noexcept { return this->tab_materials.custom_code_material; }
+
+	const bool isPrebuiltActive()   const noexcept { return this->active_material == this->tab_materials.prebuilt_material; }
+	const bool isFactoryActive()    const noexcept { return this->active_material == this->tab_materials.factory_material; }
+	const bool isCustomCodeActive() const noexcept { return this->active_material == this->tab_materials.custom_code_material; }
+
+	const void activatePrebuilt()   noexcept { this->active_material = this->tab_materials.prebuilt_material; }
+	const void activateFactory()    noexcept { this->active_material = this->tab_materials.factory_material; }
+	const void activateCustomCode() noexcept { this->active_material = this->tab_materials.custom_code_material; }
 
 	// Need to instead update the shader held on the active material
 	//const void setPrebuiltMaterialActive  () noexcept { this->active_material = this->tab_materials.prebuilt_material   ; }
