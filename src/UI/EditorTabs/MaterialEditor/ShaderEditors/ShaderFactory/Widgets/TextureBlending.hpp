bc56b368ce3543abec05bc0a2e64cc89e5fc2a26
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-05-29 21:23:39 +0100

    Add overlay_strength

--- src/UI/EditorTabs/MaterialEditor/ShaderEditors/ShaderFactory/Widgets/TextureBlending.hpp
+++ src/UI/EditorTabs/MaterialEditor/ShaderEditors/ShaderFactory/Widgets/TextureBlending.hpp
@@ -2,7 +2,13 @@
 
 #include <QWidget>
 
+#include "BaseWidgets/ToggleableTextboxes.hpp
+
 class TextureBlending : public QWidget
 {
 	Q_OBJECT;
+
+private:
+	ToggleableTextboxesWidget<1> overlay_strength;
+
 };
