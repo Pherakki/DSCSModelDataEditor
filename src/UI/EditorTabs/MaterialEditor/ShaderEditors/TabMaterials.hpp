11c629aecf2f7b53b48ed2ec23c2c3dfa052fb91
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-18 12:46:14 +0100

    Remember to store the settings on the TabMaterials

--- src/UI/EditorTabs/MaterialEditor/ShaderEditors/TabMaterials.hpp
+++ src/UI/EditorTabs/MaterialEditor/ShaderEditors/TabMaterials.hpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 
 #include "Rendering/DSCS/DataObjects/OpenGLDSCSModel.hpp"
+#include "UI/EditorTabs/MaterialEditor/ShaderEditors/ShaderFactory/ShaderGenerator/Utils.hpp"
 
 struct TabMaterials
 {
@@ -12,6 +13,7 @@ public:
     MaterialPtr prebuilt_material = nullptr;
     MaterialPtr factory_material = nullptr;
     MaterialPtr custom_code_material = nullptr;
+    FactorySettings settings;
 };
 
 typedef std::unordered_map<std::shared_ptr<Rendering::DSCS::DataObjects::OpenGLDSCSMaterial>, TabMaterials> TabMaterialsLibrary;
