70e084e05a0dfc04d3a4eab29430a5cb2278cd14
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-21 21:26:19 +0100

    Move OpenGL code to cpp, remove QOpenGLFunctions

---
+++ src/Renderer/DSCS/ShaderSystem/cgGL/OpenGLSettings/OpenGLSettings.cpp
@@ -0,0 +1,64 @@
+#include "OpenGLSettings.hpp"
+
+#include "../../../../../glad/include/glad/glad.h"
+
+namespace Rendering::DSCS::DataObjects::OpenGLSettings
+{
+	// OpenGLSettingGlEnable
+
+	void OpenGLSettingGlEnable::set()   { this->setFunc(); };
+	void OpenGLSettingGlEnable::unset() { this->unsetFunc(); };
+	void OpenGLSettingGlEnable::enable_flag()  { glEnable(this->gl_flag); }
+	void OpenGLSettingGlEnable::disable_flag() { glDisable(this->gl_flag); }
+	void OpenGLSettingGlEnable::setEnabledState(bool isEnabled)
+	{
+		if (isEnabled)
+		{
+			this->setFunc = [this]() {this->OpenGLSettingGlEnable::enable_flag(); };
+		}
+		else
+		{
+			this->setFunc = [this]() {this->OpenGLSettingGlEnable::disable_flag(); };
+		}
+		if (this->default_val)
+			this->unsetFunc = [this]() {this->OpenGLSettingGlEnable::enable_flag(); };
+		else
+			this->unsetFunc = [this]() {this->OpenGLSettingGlEnable::disable_flag(); };
+		this->isEnabled = isEnabled;
+	};
+
+	// OpenGLSettingGlAlphaFunc
+
+	void OpenGLSettingGlAlphaFunc::set()   { glAlphaFunc(this->func, this->ref); };
+	void OpenGLSettingGlAlphaFunc::unset() { };
+
+	// OpenGLSettingGlBlendFunc
+
+	void OpenGLSettingGlBlendFunc::set()   { glBlendFunc(this->src_blend_factor, this->dst_blend_factor); };
+	void OpenGLSettingGlBlendFunc::unset() { glBlendFunc(GL_ONE, GL_ZERO); };
+
+	// OpenGLSettingGlBlendEquationSeparate
+
+	void OpenGLSettingGlBlendEquationSeparate::set()   { glBlendEquationSeparate(this->rgba_blend_factor, this->rgba_blend_factor); }
+	void OpenGLSettingGlBlendEquationSeparate::unset() { };
+
+	// OpenGLSettingGlCullFace
+
+	void OpenGLSettingGlCullFace::set()   { glCullFace(this->mode); };
+	void OpenGLSettingGlCullFace::unset() { glCullFace(GL_BACK); };
+
+	// OpenGLSettingGlDepthFunc
+
+	void OpenGLSettingGlDepthFunc::set()   { glDepthFunc(this->func); };
+	void OpenGLSettingGlDepthFunc::unset() { glDepthFunc(GL_LESS); };
+
+	// OpenGLSettingGlDepthMask
+
+	void OpenGLSettingGlDepthMask::set()   { glDepthMask(this->gl_bool); };
+	void OpenGLSettingGlDepthMask::unset() { glDepthMask(true); };
+
+	// OpenGLSettingGlColorMask
+
+	void OpenGLSettingGlColorMask::set()   { glColorMask(this->r, this->g, this->b, this->a); };
+	void OpenGLSettingGlColorMask::unset() { glColorMask(1, 1, 1, 1); };
+}
--- src/Renderer/DSCS/ShaderSystem/cgGL/OpenGLSettings/OpenGLSettings.hpp
+++ src/Renderer/DSCS/ShaderSystem/cgGL/OpenGLSettings/OpenGLSettings.hpp
@@ -1,6 +1,8 @@
 #pragma once
-#include <QOpenGLFunctions_2_1>
+
+#include <array>
 #include <functional>
+#include <string>
 
 namespace Rendering::DSCS::DataObjects::OpenGLSettings
 {
@@ -16,13 +18,13 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 	};
 
 
-	class OpenGLSetting : protected QOpenGLFunctions_2_1
+	class OpenGLSetting
 	{
 		// Should hold a lookup key and a payload
 	private:
 		uint8_t id;
 	public:
-		OpenGLSetting(uint8_t id) { initializeOpenGLFunctions(); this->id = id; };
+		OpenGLSetting(uint8_t id) { this->id = id; };
 		//virtual ~OpenGLSetting() = 0;
 		virtual void set() {};
 		virtual void unset() {};
@@ -40,8 +42,8 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 	{
 	public:
 		OpenGLSettingGlEnable(uint8_t id, uint16_t gl_flag, bool isEnabled, bool default_val) : OpenGLSetting(id) { this->gl_flag = gl_flag; this->default_val = default_val; setEnabledState(isEnabled); };
-		void set() override   { this->setFunc(); };
-		void unset() override { this->unsetFunc(); };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glBool, glFlagTypes::Null, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->isEnabled), "", "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->isEnabled = std::stoi(data[0]); }
@@ -52,24 +54,9 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 
 		std::function<void(void)> setFunc;
 		std::function<void(void)> unsetFunc;
-		inline void enable_flag()  { glEnable(this->gl_flag); };
-		inline void disable_flag() { glDisable(this->gl_flag); };
-		void setEnabledState(bool isEnabled)
-		{
-			if (isEnabled)
-			{
-				this->setFunc = [this]() {this->OpenGLSettingGlEnable::enable_flag(); };
-			}
-			else
-			{
-				this->setFunc = [this]() {this->OpenGLSettingGlEnable::disable_flag(); };
-			}
-			if (this->default_val)
-				this->unsetFunc = [this]() {this->OpenGLSettingGlEnable::enable_flag(); };
-			else
-				this->unsetFunc = [this]() {this->OpenGLSettingGlEnable::disable_flag(); };
-			this->isEnabled = isEnabled;
-		};
+		inline void enable_flag();
+		inline void disable_flag();
+		void setEnabledState(bool isEnabled);
 	};
 
 
@@ -82,8 +69,8 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 		OpenGLSettingGlAlphaFunc(uint8_t id, uint16_t func, float ref) : OpenGLSetting(id) { this->func = func, this->ref = ref; };
 		uint16_t func;
 		float ref;
-		void set() override   { glAlphaFunc(this->func, this->ref); };
-		void unset() override { };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glComparison, glFlagTypes::Float, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->func), std::to_string(this->ref), "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->func = std::stoi(data[0]); this->ref = std::stof(data[1]); }
@@ -100,8 +87,8 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 		OpenGLSettingGlBlendFunc(uint8_t id, uint16_t src_blend_factor, uint16_t dst_blend_factor) : OpenGLSetting(id) { this->src_blend_factor = src_blend_factor, this->dst_blend_factor = dst_blend_factor; };
 		uint16_t src_blend_factor;
 		uint16_t dst_blend_factor;
-		void set() override { glBlendFunc(this->src_blend_factor, this->dst_blend_factor); };
-		void unset() override { glBlendFunc(GL_ONE, GL_ZERO); };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glBlendFunc, glFlagTypes::glBlendFunc, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->src_blend_factor), std::to_string(this->dst_blend_factor), "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->src_blend_factor = std::stoi(data[0]); this->dst_blend_factor = std::stoi(data[1]); }
@@ -115,8 +102,8 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 	public:
 		OpenGLSettingGlBlendEquationSeparate(uint8_t id, uint16_t rgba_blend_factor) : OpenGLSetting(id) { this->rgba_blend_factor = rgba_blend_factor; };
 		uint16_t rgba_blend_factor;
-		void set() override   { glBlendEquationSeparate(this->rgba_blend_factor, this->rgba_blend_factor); };
-		void unset() override { };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glBlendEquationSeparate, glFlagTypes::Null, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->rgba_blend_factor), "", "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->rgba_blend_factor = std::stoi(data[0]); }
@@ -131,13 +118,12 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 	public:
 		OpenGLSettingGlCullFace(uint8_t id, uint16_t mode) : OpenGLSetting(id) { this->mode = mode; };
 		uint16_t mode;
-		void set() override   { glCullFace(this->mode); };
-		void unset() override { glCullFace(GL_BACK); };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glCullFace, glFlagTypes::Null, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(mode), "", "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->mode = std::stoi(data[0]); }
 	};
-
 
 
 	// ############################### //
@@ -148,8 +134,8 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 	public:
 		OpenGLSettingGlDepthFunc(uint8_t id, uint16_t func) : OpenGLSetting(id) { this->func = func; };
 		uint16_t func;
-		void set() override   { glDepthFunc(this->func); };
-		void unset() override { glDepthFunc(GL_LESS); };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glComparison, glFlagTypes::Null, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->func), "", "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->func = std::stoi(data[0]); }
@@ -164,8 +150,8 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 	public:
 		OpenGLSettingGlDepthMask(uint8_t id, bool gl_bool) : OpenGLSetting(id) { this->gl_bool = gl_bool; };
 		bool gl_bool;
-		void set() override   { glDepthMask(this->gl_bool); };
-		void unset() override { glDepthMask(true); };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glBool, glFlagTypes::Null, glFlagTypes::Null, glFlagTypes::Null }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->gl_bool), "", "", "" }; }
 		void setData(const std::array<std::string, 4>& data) override { this->gl_bool = std::stoi(data[0]); }
@@ -183,12 +169,10 @@ namespace Rendering::DSCS::DataObjects::OpenGLSettings
 		bool g;
 		bool b;
 		bool a;
-		void set() override   { glColorMask(this->r, this->g, this->b, this->a); };
-		void unset() override { glColorMask(1, 1, 1, 1); };
+		void set() override;
+		void unset() override;
 		std::array<glFlagTypes, 4> getFlags() override { return { glFlagTypes::glBool, glFlagTypes::glBool, glFlagTypes::glBool, glFlagTypes::glBool }; }
 		std::array<std::string, 4> getData()  override { return { std::to_string(this->r), std::to_string(this->g), std::to_string(this->b) , std::to_string(this->a) }; }
 		void setData(const std::array<std::string, 4>& data) override { this->r = std::stoi(data[0]); this->g = std::stoi(data[1]); this->b = std::stoi(data[2]); this->a = std::stoi(data[3]); }
 	};
-
-
 }
