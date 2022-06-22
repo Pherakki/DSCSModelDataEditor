f6614b050ebc0bf601f12eaec0d665296b666bfa
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-22 18:04:25 +0100

    Remove comments about renaming "Renderer.hpp" and including GLAD

--- src/UI/RenderWidget.hpp
+++ src/UI/RenderWidget.hpp
@@ -18,10 +18,6 @@
 #include "../Renderer/DSCS/ShaderSystem/cgGL/cgGLShaderObject.hpp"
 #include "../Utils/Matrix.hpp"
 
-
-// Need to move most of this into the Renderer at some point, and move the AnimationBuffer code from there into a proper place...
-// This might be useful for having the Renderer be independent of Qt5 code: https://stackoverflow.com/questions/33635001/qopenglwidget-with-shared-opengl-context
-
 namespace CustomWidgets
 {
 	//class LampModel : protected QOpenGLFunctions_2_1
