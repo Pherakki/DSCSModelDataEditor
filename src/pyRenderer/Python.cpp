5ff578323575b7ecae8c4e421dbcdef5ca031595
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-24 22:07:42 +0100

    Add initial Python bindings

--- src/pyRenderer/Python.cpp
+++ src/pyRenderer/Python.cpp
@@ -5,7 +5,52 @@
 using namespace boost::python;
 using namespace Rendering::DSCS;
 
+class pyRenderer
+{
+private:
+	Renderer renderer;
+public:
+	void initRenderer()
+	{
+		renderer.initRenderer();
+	}
+
+	void loadModel(std::string filepath)
+	{
+		renderer.loadModel(filepath);
+	}
+
+	void recalculateGlobalUniforms()
+	{
+		renderer.recalculateGlobalUniforms();
+	}
+
+	void refreshRenderSettings()
+	{
+		renderer.refreshRenderSettings();
+	}
+
+	void render()
+	{
+		renderer.render();
+	}
+
+	void setCameraPosition(list pos)
+	{
+		auto x = extract<float>(pos[0]);
+		auto y = extract<float>(pos[1]);
+		auto z = extract<float>(pos[2]);
+		renderer.camera.setPosition({x,y,z});
+	}
+};
+
 BOOST_PYTHON_MODULE(pyDSCSRenderer)
 {
-
+	class_<pyRenderer, boost::noncopyable>("DSCSRenderer")
+		.def("initRenderer",              &pyRenderer::initRenderer)
+		.def("refreshRenderSettings"    , &pyRenderer::refreshRenderSettings)
+		.def("recalculateGlobalUniforms", &pyRenderer::recalculateGlobalUniforms)
+		.def("render",                    &pyRenderer::render)
+		.def("loadModel",                 &pyRenderer::loadModel, arg("filepath"))
+		.def("setCameraPosition",         &pyRenderer::setCameraPosition, arg("pos"));
 }
